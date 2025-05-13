6ba8004b24ba77e01cb59f9a6502cdd75b3eae79
matheus-r-silva <matheus.rocha@ads.uniceplac.edu.br>
2025-05-13 11:04:49 -0300

    Update TesteConta.java

--- TesteConta.java
+++ TesteConta.java
@@ -3,11 +3,24 @@ public class TesteContas {
 	 * executando metodos de depositar e sacar e ao final apresentando como saida o resultado
 	 * das operações*/
 	public static void main(String[] args) {
-		var conta = new ContaCorrente("Jonas", 1500);
+		var conta = new ContaCorrente("Joze");
+		var contap = new ContaPoupanca("Aline");
+		
+		
 		conta.depositar(1500);
 		conta.sacar(250);
 		
-		System.out.println(conta.toString());
-		//System.out.println("Titular: " + conta.getTitular() + " e saldo" + conta.getSaldo());
+		/*contap.depositar(1500);
+		  contap.sacar(500);
+		 * */
+		
+		
+		contap.depositar(490);
+		contap.sacar(500);
+		
+		
+		System.out.println(conta);
+		System.out.println(contap);
+		
 	}
 }
